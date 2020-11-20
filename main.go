d664ee7083da99ac4617f74a97c80ebaf74b2c8a
Garin Prakoso <gerin.rin@gmail.com>
2020-11-20 21:47:52 +0700

    tambahin init DB

--- main.go
+++ main.go
@@ -3,27 +3,36 @@ package main
 import (
 	"fmt"
 	"log"
+	"warehousely/config"
+	"warehousely/config/database/postgres"
 
 	"github.com/gin-gonic/gin"
 )
+
 const (
 	ServerHost = "localhost"
 	ServerPort = "8000"
 )
-type Goods struct {
-	ID int
-	Name string
-	Type string
-	Stock int
 
+type Goods struct {
+	ID    int
+	Name  string
+	Type  string
+	Stock int
 }
+
 func main() {
+	// set up environment variable
+	config.SetEnvironmentVariable()
+	db := postgres.InitPostgresDatabase()
+	fmt.Println(db)
 	g := gin.Default()
 
 	g.GET("/", func(c *gin.Context) {
 		c.JSON(200, "Hello There")
 	})
-	log.Printf("Starting server at %s:%s",ServerHost,ServerPort)
-	source := fmt.Sprintf("%s:%s",ServerHost,ServerPort)
+
+	log.Printf("Starting server at %s:%s", ServerHost, ServerPort)
+	source := fmt.Sprintf("%s:%s", config.MAIN_SERVER_HOST, config.MAIN_SERVER_PORT)
 	g.Run(source)
 }
