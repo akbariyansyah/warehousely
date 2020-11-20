48fec30dfd9df6482d8ace322c2ede1ed5a2de7c
akbariyansyah <riyansean21366@gmail.com>
2020-11-20 20:56:42 +0700

    added gin gionic

--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
+	"github.com/gin-gonic/gin"
+)
+const (
+	ServerHost = "localhost"
+	ServerPort = "8000"
+)
+type Goods struct {
+	ID int
+	Name string
+	Type string
+	Stock int
+
+}
 func main() {
-	
-	fmt.Println("Hello World")
+	g := gin.Default()
+
+	g.GET("/", func(c *gin.Context) {
+		c.JSON(200, "Hello There")
+	})
+	log.Printf("Starting server at %s:%s",ServerHost,ServerPort)
+	source := fmt.Sprintf("%s:%s",ServerHost,ServerPort)
+	g.Run(source)
 }
