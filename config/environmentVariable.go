2993fb3c098b2bd1a997ec6b6b6c8d11b8cf4852
Garin Prakoso <gerin.rin@gmail.com>
2020-11-22 17:53:11 +0700

    change gin to release mode

--- config/environmentVariable.go
+++ config/environmentVariable.go
@@ -14,17 +14,11 @@ var MAIN_SERVER_HOST,
 	DB_NAME string
 
 func SetEnvironmentVariable() {
-	MAIN_SERVER_HOST = "127.0.0.1"
-	PORT = os.Getenv(PORT)
+	// MAIN_SERVER_HOST = "127.0.0.1"
+	PORT = os.Getenv("PORT")
 	if PORT == "" {
 		PORT = "8000"
 	}
-
-	// DB_USER = "postgres"
-	// DB_PASSWORD = "P@ssW02d123"
-	// DB_HOST = "127.0.0.1"
-	// DB_PORT = "5432"
-	// DB_NAME = "warehousely"
 
 	DB_USER = "root"
 	DB_PASSWORD = "admin"
--- main.go
+++ main.go
@@ -12,10 +12,10 @@ import (
 func main() {
 	// set up environment variable
 	config.SetEnvironmentVariable()
+	gin.SetMode(gin.ReleaseMode)
 
 	db := postgres.InitPostgresDatabase()
 	g := gin.New()
-	gin.SetMode(gin.ReleaseMode)
 
 	router.InitRouter(g, db)
 
